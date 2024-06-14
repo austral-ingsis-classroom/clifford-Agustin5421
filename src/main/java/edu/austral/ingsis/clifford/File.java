100039af12eb4cd512ed7512b28e656d3100c50a
Agustin <almartinez1@mail.austral.edu.ar>
2024-06-14 14:34:51 -0300

    build fixes

--- src/main/java/edu/austral/ingsis/clifford/File.java
+++ src/main/java/edu/austral/ingsis/clifford/File.java
@@ -1,21 +1,61 @@
 package edu.austral.ingsis.clifford;
 
 import java.util.List;
+import java.util.Objects;
 
-public record File(String name, FileSystemComponent father) implements FileSystemComponent {
+public final class File implements FileSystemComponent {
+    private final String name;
+    private final FileSystemComponent father;
 
-  @Override
-  public List<FileSystemComponent> getChildren() {
-    throw new UnsupportedOperationException("Files don´t have children!");
-  }
+    public File(String name, FileSystemComponent father) {
+        this.name = name;
+        this.father = father;
+    }
 
-  @Override
-  public void add(FileSystemComponent component) {
-    throw new UnsupportedOperationException("Files can´t add files!");
-  }
+    @Override
+    public List<FileSystemComponent> getChildren() {
+        throw new UnsupportedOperationException("Files do not have children!");
+    }
 
-  @Override
-  public void remove(String name) {
-    throw new UnsupportedOperationException("Files can´t remove files!");
-  }
+    @Override
+    public void add(FileSystemComponent component) {
+        throw new UnsupportedOperationException("Files cannot add files!");
+    }
+
+    @Override
+    public void remove(String name) {
+        throw new UnsupportedOperationException("Files cannot remove files!");
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public FileSystemComponent father() {
+        return father;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (File) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.father, that.father);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, father);
+    }
+
+    @Override
+    public String toString() {
+        return "File[" +
+                "name=" + name + ", " +
+                "father=" + father + ']';
+    }
+
 }
